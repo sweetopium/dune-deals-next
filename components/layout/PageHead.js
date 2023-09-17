c946a0a542e7edb0c7a5901b2de3b70499961466
Konstantin Rusanov <gotoart@yandex.ru>
2023-09-17 15:34:49 +0300

    favicon

--- components/layout/PageHead.js
+++ components/layout/PageHead.js
@@ -5,7 +5,7 @@ const PageHead = ({ headTitle }) => {
         <>
             <Head>
                 <title>
-                    {headTitle ? headTitle : "Sala - Multipurpose Startup SaaS NextJS Template"}
+                    {headTitle ? headTitle : "DuneDeals"}
 
                 </title>
                 <link
