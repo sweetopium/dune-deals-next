e9ccc83307d8917c63cc4ac05f32ef03c8cbda67
Konstantin Rusanov <gotoart@yandex.ru>
2023-09-25 09:33:29 +0300

    fix gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
-
 import '../public/assets/libs/bootstrap/css/bootstrap.min.css';
 import '../public/assets/libs/line-awesome/css/line-awesome.min.css';
-// import '../public/assets/libs/swiper/swiper-bundle.min.css'
-
 import 'animate.css';
 import { useEffect, useState } from "react";
 import 'swiper/css';
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import '../public/assets/css/style.css';
+import Script from "next/script";
 export default function App({ Component, pageProps }) {
     const [loading, setLoading] = useState(true);
     useEffect(() => {
@@ -19,6 +17,13 @@ export default function App({ Component, pageProps }) {
     }, []);
     return (
         <>
+            <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-NFKW7Z54');`
+                }} />
             {!loading ? (
                 <Component {...pageProps} />
             ) : (
--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,12 @@ import Dating_Section1 from "@/components/sections/homepage/Dating/Dating_Sectio
 import App_Showcase_Section2 from "@/components/sections/homepage/App_Showcase/App_Showcase_Section2";
 import Pet_Section3 from "@/components/sections/homepage/Pet/Pet_Section3";
 import Head from 'next/head'
-import Script from "next/script";
 
 export default function HomeDating() {
     return (
         <>
             <Head>
                 <title>DuneDeals - Explore the best offers & save in Dubai</title>
-
-                <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
-                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                            })(window,document,'script','dataLayer','GTM-NFKW7Z54');`
-                }} />
-
             </Head>
             <Layout headerClass="is-transparent" siteContentClass="pt0 pb0">
                 <Dating_Section1/>
